f9031bcc2758320a76d716d9181e17fab1c570fb
malindyr <malin.dyrnes2@gmail.com>
2024-08-28 15:32:06 +0200

    .

---
+++ BossFight__(Inheritance_with_notes)/Gitignore_test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._06._24__bossFight_take_2_
+{
+    internal class Gitignore_test
+    {
+    }
+}
+
