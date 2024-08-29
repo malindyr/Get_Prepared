4a4d33358b9f40dc8484af149f7deae664a6074c
malindyr <malin.dyrnes2@gmail.com>
2024-08-29 09:17:59 +0200

    More troubleshooting/testing

--- TroubleShootingGit/obj/Release/net8.0/TroubleShootingGit.AssemblyInfo.cs
+++ TroubleShootingGit/obj/Release/net8.0/TroubleShootingGit.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TroubleShootingGit")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90a008a600270c5cffa5aa9986317e8bccf36dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TroubleShootingGit")]
