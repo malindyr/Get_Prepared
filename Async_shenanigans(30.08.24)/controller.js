530d17083430ab4886498238a4585d7b8351b0df
malindyr <malin.dyrnes2@gmail.com>
2024-08-30 10:23:22 +0200

    Promise-based and async function
    Testing out and getting familiar with both async and promise-based functions.

--- Async_shenanigans(30.08.24)/controller.js
+++ Async_shenanigans(30.08.24)/controller.js
@@ -1,5 +1,31 @@
 
+fetchData1();
 
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-.then(response => console.log(response))
+function fetchData1(){
+
+fetch("https://pokeapi.co/api/v2/pokemon/mewtwo")
+.then(response => {
+    if(!response.ok){
+        throw new Error("could not fetch resource :(");
+    }
+    return response.json();
+})
+.then(data => console.log(data.name))
 .catch(error => console.error(error));
+}
+
+fetchData();
+
+async function fetchData(){
+    try{
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+        if(!response.ok){
+            throw new Error("could not fetch resource (async func)")
+        }
+        const data = await response.json();
+        console.log(data.name);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
