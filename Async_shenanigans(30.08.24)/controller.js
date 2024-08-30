42596d6f24731c7e6c3a728afe68ec4c26c48ce3
malindyr <malin.dyrnes2@gmail.com>
2024-08-30 12:45:29 +0200

    connected to my local db
    Connected my tiny test program to my api!

--- Async_shenanigans(30.08.24)/controller.js
+++ Async_shenanigans(30.08.24)/controller.js
@@ -1,6 +1,5 @@
 
 fetchData1();
-
 function fetchData1(){
 
 fetch("https://pokeapi.co/api/v2/pokemon/mewtwo")
@@ -13,12 +12,28 @@ fetch("https://pokeapi.co/api/v2/pokemon/mewtwo")
 .then(data => console.log(data.name))
 .catch(error => console.error(error));
 }
-
-fetchData();
-
+//fetchData();
 async function fetchData(){
     try{
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+            throw new Error("could not fetch resource (async func)")
+        }
+        const data = await response.json();
+        console.log(data.name);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+fetchDataFromLocalDb();
+async function fetchDataFromLocalDb(){
+    try{
+        const response = await fetch(``);
+
         if(!response.ok){
             throw new Error("could not fetch resource (async func)")
         }
