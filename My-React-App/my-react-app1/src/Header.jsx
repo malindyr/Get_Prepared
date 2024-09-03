bf7941149bba544c560d40304be99cad18959c02
malindyr <malin.dyrnes2@gmail.com>
2024-09-03 15:41:20 +0200

    Learning about react
    In the starting phases of getting comfortable with using react!

---
+++ My-React-App/my-react-app1/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>my website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li>about</li>
+                    <li>services</li>
+                    <li>contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
