0090833c166a6f2e06d8918677b911e979b59429
malindyr <malin.dyrnes2@gmail.com>
2024-09-11 13:46:13 +0200

    testing things out
    learning react

---
+++ reactproject1/react0910/src/App.jsx
@@ -0,0 +1,17 @@
+//rafce
+//rafc
+//rfc
+
+import React from 'react'
+
+const App = () => {
+  const name = 'John'
+  return (
+    <>
+    <div className='text-5xl'>App</div>
+    <p>hello {name}</p>
+    </>
+  )
+}
+
+export default App
