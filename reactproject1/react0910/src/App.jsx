c0ff5bd3a1c15e9d30e1f47e359b4f0514704c8b
malindyr <malin.dyrnes2@gmail.com>
2024-09-15 11:45:13 +0200

    added jobLoader
    added jobLoader using useLoaderData from react dom. And added a lot of visuals to the job page.

--- reactproject1/react0910/src/App.jsx
+++ reactproject1/react0910/src/App.jsx
@@ -8,14 +8,14 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage/>}/>
       <Route path='/jobs' element={<JobsPage/>}/>
-      <Route path='/jobs/:id' element={<JobPage/>}/> {/* : here means it's dynamic*/}
+      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/> {/* : here means it's dynamic*/}
       <Route path='/*' element={<NotFoundPage/>}/>
     </Route>)
 )
