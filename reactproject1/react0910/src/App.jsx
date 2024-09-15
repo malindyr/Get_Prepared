7f54552e2b1bd749066498b9ffad34d2fd858f9f
malindyr <malin.dyrnes2@gmail.com>
2024-09-15 16:18:37 +0200

    POST functionality for new job

--- reactproject1/react0910/src/App.jsx
+++ reactproject1/react0910/src/App.jsx
@@ -1,6 +1,3 @@
-//rafce
-//rafc
-//rfc
 
 import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
@@ -11,18 +8,32 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage/>}/>
-      <Route path='/jobs' element={<JobsPage/>}/>
-        <Route path='/add-job' element={<AddJobPage/>}/>
-      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/> {/* : here means it's dynamic*/}
-      <Route path='/*' element={<NotFoundPage/>}/>
-    </Route>)
-)
 
 const App = () => {
+
+  //add new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage/>}/>
+      <Route path='/jobs' element={<JobsPage/>}/>
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}/>
+      <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/> {/* : here means it's dynamic*/}
+      <Route path='/*' element={<NotFoundPage/>}/>
+      </Route>
+      )
+  );
   return <RouterProvider router={router}/>
 }
 
