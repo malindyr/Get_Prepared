084dcea72c1bf17c45ba90940b4425178cddda61
malindyr <malin.dyrnes2@gmail.com>
2024-09-23 13:01:51 +0200

    starting a tiny new project

---
+++ todoapp/src/components/ToDoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+const ToDoList = () => {
+
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  return (
+    <>ToDoList</>
+  )
+}
+
+export default ToDoList
