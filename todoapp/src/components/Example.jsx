2f41ee4c76d2e686f08ce0f7a7831f023eb12493
malindyr <malin.dyrnes2@gmail.com>
2024-09-24 13:38:35 +0200

    more testing and playing around

--- todoapp/src/components/Example.jsx
+++ todoapp/src/components/Example.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from 'react'
 import Names from "./Names";
 
@@ -7,25 +7,37 @@ const Example = () => {
 
     const [inputA, setInputA] = useState('');
     const [inputB, setInputB] = useState('');
+    const [namesA, setNamesA] = useState([])
 
     const handleChange = (e) => {
         setInputA(e.target.value);
     }
 
-    const getNames = () => {
-
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setNamesA([...namesA, inputB])
+        
     }
+
+    useEffect(() => {
+        console.log(namesA); // Logs the updated array after state changes
+      }, [namesA]);
+
 
   return (
     <>
         <input type="text" 
             onChange={handleChange}/>
 
+    <form onSubmit={onSubmit}>
         <input type="text"
             onChange={(e) => setInputB((e.target.value))}/>
 
             <div>{inputA}</div>
             <div>{inputB}</div>
+
+            <button type="submit">submit</button>
+    </form>
 
         <ul>
             {Names.map((name, index) => (
