2eaa01f38802279b151a455ec4ac6d6cbfd88bf7
malindyr <malin.dyrnes2@gmail.com>
2024-09-30 12:52:07 +0200

    cout & cin

--- C++.30.09.24/C++.30.09.24.cpp
+++ C++.30.09.24/C++.30.09.24.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
+using namespace std;
 
-int main() 
-{
-	int file_size = 0;//always initialize variables before using them, garbage will be in current memory otherwise
-	int counter = 1;
+int main() {
+	int x;
+	int y;
 
-	int a = 1;
-	int b = 2;
+	cout << "enter a number" << endl;
+	cin >> x;
+	cin >> y;
+	cout << x << endl;
+	cout << y << endl; 
+	cout << x + y << endl;
 
-	int temp = a;
-
-	a = b;
-	b = temp;
-
-	std::cout << a;
-	
-	double sales = 9.99;
-	//std::cout << file_size;//standard library, character out, this line is called a statement, terminate with ;
-	return 0; //0 tells the operating system that our program is going to terminate correctly. if any other value is returned, that means our program encountered an error
 }
-
-
-//to run this program, first we have to compile this code to machine code that can be run by the computers operating system.
-//machine code is basically the native language that a computers operating system understands, and its different from one operating system to another.
-//so if we compile this code on a windows machine, we get an executable, but that exe only runs on windows. not on mac or linux
